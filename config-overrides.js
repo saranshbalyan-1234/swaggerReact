0b81779d9e5c2b9ec70c58898242ed5835f26447
saransh balyan <saransh@kditsolutions.com>
2022-06-05 18:22:31 +0530

    Revert "uglify"
    This reverts commit a53834f1a8e5f1f902151bf22374ecab164709e3.

--- config-overrides.js
+++
@@ -1,6 +0,0 @@
-const rewireUglifyjs = require("react-app-rewire-uglifyjs");
-module.exports = function override(config, env) {
-  // New config, e.g. config.plugins.push...
-  config = rewireUglifyjs(config);
-  return config;
-};
